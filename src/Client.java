2e275f727638835dc0cb81579f1982c51db3922b
greekbrains <info@geekbrains.ru>
2021-02-19 18:57:41 +0300

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,14 @@
+
+    import java.awt.event.ActionEvent;
+import java.io.*;
+import java.net.Proxy;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+    public class Client {
+      
+    }
+
+
---
+++ src/ServerChat.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ServerChat {
+
+}
+
