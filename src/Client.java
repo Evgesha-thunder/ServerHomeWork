f4935a48a06b75973c1cad1210e75fe8017c277e
greekbrains <info@geekbrains.ru>
2021-02-19 18:58:24 +0300

    homeWork

--- src/Client.java
+++ src/Client.java
@@ -8,7 +8,43 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
     public class Client {
-      
+        private static final int PORT = 8190;
+        private static Socket socket;
+        private static final String IP_ADDRESS = "localhost";
+
+        public static void main(String[] args) {
+            try {
+                socket = new Socket(IP_ADDRESS, PORT);
+
+
+                Scanner sc = new Scanner(socket.getInputStream());
+
+                Scanner scanner1 = new Scanner(System.in);
+                String str = scanner1.nextLine();
+
+                new Thread(() -> {
+
+                    try {
+
+
+                        PrintWriter out2 = new PrintWriter(socket.getOutputStream(),true);
+                        out2.println(str);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }).start();
+
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 
--- src/ServerChat.java
+++ src/ServerChat.java
@@ -5,6 +5,56 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class ServerChat {
+    private static final int PORT = 8190;
+    private static ServerSocket server;
+    private static Socket socket;
+
+    public static void main(String[] args) {
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Server started");
+            socket = server.accept();
+            System.out.println("Client connected");
+            Scanner sc = new Scanner(socket.getInputStream());
+            PrintWriter out1 = new PrintWriter(socket.getOutputStream(), true);
+            while (true) {
+
+                try {
+
+                    String str = sc.nextLine();
+                    out1.println("ECHO: ");
+
+                    if (str.equals("/end")) {
+                        System.out.println("Client disconnected");
+
+
+                    }
+
+                    System.out.println("Client: " + str);
+                    String str2 = sc.nextLine();
+                    out1.println(str2);
+
+                } catch (Exception e) {
+
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
 
